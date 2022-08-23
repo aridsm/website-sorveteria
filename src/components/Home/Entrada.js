aab450fb3c930f427e5ee6f8cba82145f99d1d0e
aridsm <aniemorelanie@gmail.com>
2022-08-23 18:52:01 -0300

    funcionamento + link entrada

--- src/components/Home/Entrada.js
+++ src/components/Home/Entrada.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import classes from './Entrada.module.css'
+import { ReactComponent as IconArrow } from '../../assets/arrow-right.svg'
 
 const Entrada = () => {
   return (
     <section className={classes.entrada}>
       <div className={classes.detalhe}></div>
       <div className={classes.principal}>
-        <p className={classes.funcionamento}>Segunda a sexta - das 15h as 21h / SABADO E DOMINGO - 15h as 23h </p>
+        <p className={classes.funcionamento}><span>Segunda a sexta - das 15h as 21h</span> / <span>Sabado e domingo - 15h as 23h</span> </p>
         <h1>Lorem <span>i</span><span>p</span><span>s</span><span>u</span><span>m</span> <br /> dolor sit amet</h1>
         <p className={classes.descricao}>All the Lorem Ipsum generators on the <br />
           Internet tend to repeat</p>
-        <Link to='/lojas'>Encontre a loja mais perto de voce --</Link>
+        <Link to='/lojas'>Encontre a loja mais perto de voce <IconArrow /></Link>
       </div>
       <ul className={classes.caracts}>
         <li>
