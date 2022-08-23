884a24d1b902a449c1dfd39f2ddc5f889d70539f
aridsm <aniemorelanie@gmail.com>
2022-08-23 18:06:24 -0300

    seçao vem ai

--- src/components/Home/VemAi.js
+++ src/components/Home/VemAi.js
@@ -5,16 +5,16 @@ import ImgSorvete from '../../assets/casquinhas.png'
 const VemAi = () => {
   return (
     <section className={`container ${classes.vemAi}`}>
-      <div>
-        <h2>Vem ai</h2>
+      <div className={classes.txt}>
+        <h2 className={classes.titulo}>Vem ai</h2>
         <p>Lorem ipsum dolor sit amet lorem ipsum dolor sit amet</p>
       </div>
       <div className={classes.img}>
         <img src={ImgSorvete} alt='3 casquinhas de sorvete' />
       </div>
-      <div>
+      <div className={classes.sabores}>
         <h3>Nos sabores</h3>
-        <ul>
+        <ul className={classes.saboresLista}>
           <li>Chocolate</li>
           <li>Morango</li>
           <li>Caramelo</li>
