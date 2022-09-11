40d748fd400fdd8cab3fd4ebadf4fd54f78eb5bb
aridsm <aniemorelanie@gmail.com>
2022-09-11 10:50:50 -0300

    ajustar gap

--- src/components/Home/Entrada.js
+++ src/components/Home/Entrada.js
@@ -1,46 +1,52 @@
-import React, { useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import classes from './Entrada.module.css'
-import ImgEntrada from "../../assets/sorvete-entrada.webp"
-import { ReactComponent as ArrowSvg } from '../../assets/arrow-right.svg'
-import "aos/dist/aos.css"
-import AOS from 'aos'
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import classes from "./Entrada.module.css";
+import ImgEntrada from "../../assets/sorvete-entrada.webp";
+import { ReactComponent as ArrowSvg } from "../../assets/arrow-right.svg";
+import "aos/dist/aos.css";
+import AOS from "aos";
 
 const Entrada = () => {
-
   useEffect(() => {
     AOS.init({
       duration: 800,
-      offset: 0
-    })
-  }, [])
+      offset: 0,
+    });
+  }, []);
 
   return (
     <section className={classes.entrada}>
       <div className={classes.container}>
         <div className={classes.principal} data-aos="fade-down">
-          <p className={classes.funcionamento}>Segunda a sexta - 15h as 21h / Sábado e domingo - 15h as 23h </p>
-          <h1>Lorem <span>i</span><span>p</span><span>s</span><span>u</span><span>m</span> <br /> dolor sit amet</h1>
-          <p className={classes.descricao}>Nulla ultricies placerat posuere. Fusce sagittis ligula eu <br/> porttitor. Donec ultricies et felis at euismod.</p>
-          <Link to='/lojas'>Encontre a loja mais perto de você <ArrowSvg/></Link>
+          <p className={classes.funcionamento}>
+            Segunda a sexta - 15h as 21h / Sábado e domingo - 15h as 23h{" "}
+          </p>
+          <h1>
+            Lorem <span>i</span>
+            <span>p</span>
+            <span>s</span>
+            <span>u</span>
+            <span>m</span> <br /> dolor sit amet
+          </h1>
+          <p className={classes.descricao}>
+            Nulla ultricies placerat posuere. Fusce sagittis ligula eu <br />{" "}
+            porttitor. Donec ultricies et felis at euismod.
+          </p>
+          <Link to="/lojas">
+            Encontre a loja mais perto de você <ArrowSvg />
+          </Link>
         </div>
       </div>
-      <div className={classes.img}data-aos="fade-up">
-          <img src={ImgEntrada} alt='Casquinha de sorvete' />
+      <div className={classes.img} data-aos="fade-up">
+        <img src={ImgEntrada} alt="Casquinha de sorvete" />
       </div>
       <ul className={classes.caracts} data-aos="fade-down">
-        <li>
-        Sed augue erat, congue et
-        </li>
-        <li>
-        Aliquam elementum arcu ut
-        </li>
-        <li>
-        Duis pharetra maximus lorem
-        </li>
+        <li>Sed augue erat, congue et</li>
+        <li>Aliquam elementum arcu ut</li>
+        <li>Duis pharetra maximus lorem</li>
       </ul>
     </section>
-  )
-}
+  );
+};
 
-export default Entrada
+export default Entrada;
