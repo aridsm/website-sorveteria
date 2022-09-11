abe1926c8b730d52f9b3098ef8d06e9c358f8dd2
aridsm <aniemorelanie@gmail.com>
2022-09-11 10:36:52 -0300

    slide aumentar gap e corrigir margens

--- src/components/Utility/Slider.js
+++ src/components/Utility/Slider.js
@@ -1,25 +1,32 @@
-import React, { memo, useCallback, useEffect, useRef, useState } from 'react'
-import classes from './Slider.module.css'
-import {ReactComponent as IconArrowRight} from '../../assets/arrow-right-short.svg'
-import {ReactComponent as IconArrowLeft} from '../../assets/arrow-left-short.svg'
-import PropTypes from 'prop-types';
-import "aos/dist/aos.css"
-import AOS from 'aos'
-import SliderModal from './SliderModal';
+import React, { memo, useCallback, useEffect, useRef, useState } from "react";
+import classes from "./Slider.module.css";
+import { ReactComponent as IconArrowRight } from "../../assets/arrow-right-short.svg";
+import { ReactComponent as IconArrowLeft } from "../../assets/arrow-left-short.svg";
+import PropTypes from "prop-types";
+import "aos/dist/aos.css";
+import AOS from "aos";
+import SliderModal from "./SliderModal";
 
-const ItemsSlider = memo(({itemWidth, item, openModal}) => {
+const ItemsSlider = memo(({ itemWidth, item, openModal }) => {
   return (
-      <li style={{minWidth:`${itemWidth}px`}}>
-        <button className={classes.btnSlideImg} onClick={() => openModal(item)} draggable='false'>
-          <img src={require(`../../assets/img-produtos/${item.img}.webp`)} alt={item.nome} draggable='false' />
-          <p className={classes.nomeItem}>{item.nome}</p>
-        </button>
-      </li>
-      )
-})
+    <li style={{ minWidth: `${itemWidth}px` }}>
+      <button
+        className={classes.btnSlideImg}
+        onClick={() => openModal(item)}
+        draggable="false"
+      >
+        <img
+          src={require(`../../assets/img-produtos/${item.img}.webp`)}
+          alt={item.nome}
+          draggable="false"
+        />
+        <p className={classes.nomeItem}>{item.nome}</p>
+      </button>
+    </li>
+  );
+});
 
-const Slider = ({items, colorsBtn}) => {
-
+const Slider = ({ items, colorsBtn }) => {
   const totalItens = items.length;
   const [itensShown, setItensShown] = useState(5);
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -31,120 +38,146 @@ const Slider = ({items, colorsBtn}) => {
   useEffect(() => {
     AOS.init({
       duration: 800,
-      offset: 0
-    })
-  }, [])
+      offset: 0,
+    });
+  }, []);
 
   useEffect(() => {
- 
     setItemWidth(slideRef.current.offsetWidth / itensShown);
 
-    slideRef.current.style.transform = `translateX(${-(currentIndex * itemWidth)}px)`
+    slideRef.current.style.transform = `translateX(${-(
+      currentIndex * itemWidth
+    )}px)`;
+  }, [currentIndex, itemWidth, itensShown]);
 
-  }, [currentIndex, itemWidth, itensShown])
-
-const debounce = useCallback((func, wait, immediate) => {
-	let timeout;
-	return function() {
-		const context = this, args = arguments;
-		const later = function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		};
-		const callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
-	};
-}, []);
+  const debounce = useCallback((func, wait, immediate) => {
+    let timeout;
+    return function () {
+      const context = this,
+        args = arguments;
+      const later = function () {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      const callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  }, []);
 
   useEffect(() => {
     const mediaQuerySizes = [
-      {windowSize: 900, itensToShow: 4}, 
-      {windowSize: 750, itensToShow: 3}, 
-      {windowSize: 550, itensToShow: 2}, 
-      {windowSize: 360, itensToShow: 1}, 
-     ]
+      { windowSize: 900, itensToShow: 4 },
+      { windowSize: 750, itensToShow: 3 },
+      { windowSize: 550, itensToShow: 2 },
+      { windowSize: 360, itensToShow: 1 },
+    ];
 
     const changeSlideItemsToShown = debounce(() => {
+      const reverseOrderMediaQueries = [...mediaQuerySizes].reverse();
 
-      const reverseOrderMediaQueries = [...mediaQuerySizes].reverse()
-
-      const windowMatch = reverseOrderMediaQueries.find(mediaQuery => mediaQuery.windowSize >= window.innerWidth);  
+      const windowMatch = reverseOrderMediaQueries.find(
+        (mediaQuery) => mediaQuery.windowSize >= window.innerWidth
+      );
 
       if (windowMatch) {
-        setItensShown(windowMatch.itensToShow)
+        setItensShown(windowMatch.itensToShow);
       } else {
-        setItensShown(5)
+        setItensShown(5);
       }
 
-      setItemWidth(slideRef.current.offsetWidth / itensShown)
+      setItemWidth(slideRef.current.offsetWidth / itensShown);
       setCurrentIndex(0);
-
-    }, 300)
+    }, 300);
 
     changeSlideItemsToShown();
 
-    window.addEventListener('resize', changeSlideItemsToShown)
+    window.addEventListener("resize", changeSlideItemsToShown);
     return () => {
-      window.removeEventListener('resize', changeSlideItemsToShown)
-    }
-  }, [debounce, itensShown])
-
+      window.removeEventListener("resize", changeSlideItemsToShown);
+    };
+  }, [debounce, itensShown]);
 
   const nextSlideHandler = () => {
-    if(currentIndex > totalItens - itensShown - 1) return
-    setCurrentIndex(state => state + 1)
-  }
+    if (currentIndex > totalItens - itensShown - 1) return;
+    setCurrentIndex((state) => state + 1);
+  };
 
   const previousSlideHandler = () => {
-    if(currentIndex === 0) return;
-    setCurrentIndex(state => state - 1)
-  } 
+    if (currentIndex === 0) return;
+    setCurrentIndex((state) => state - 1);
+  };
 
   const handleOpenModal = useCallback((item) => {
     setModalIsShown(true);
-    setCurrentItemModal(item)
-  }, [])
+    setCurrentItemModal(item);
+  }, []);
 
   const handleCloseModal = () => {
-    setModalIsShown(false)
-  }
+    setModalIsShown(false);
+  };
 
-  const canSlide = totalItens <= itensShown
+  const canSlide = totalItens <= itensShown;
 
   return (
     <>
-    {modalIsShown && <SliderModal currentItemModal={currentItemModal} onClose={handleCloseModal}/>}
-    <div className={classes.wrapSlider} data-aos="fade-up">
-      {!canSlide && <>
-        <button className={classes.btnAnt} onClick={previousSlideHandler} style={colorsBtn} aria-label='item anterior'><IconArrowLeft/></button>
-        <button className={classes.btnDep} onClick={nextSlideHandler} style={colorsBtn} aria-label='item posterior'><IconArrowRight/></button>
-      </>}
-    <div className={classes.sliderContainer}>
-        <ul className={classes.slider} ref={slideRef}>
-          {items.map(item =>
-            <ItemsSlider key={item.id} item={item} openModal={handleOpenModal} itemWidth={itemWidth}/>
-            )}
-        </ul>
+      {modalIsShown && (
+        <SliderModal
+          currentItemModal={currentItemModal}
+          onClose={handleCloseModal}
+        />
+      )}
+      <div className={classes.wrapSlider} data-aos="fade-up">
+        {!canSlide && (
+          <>
+            <button
+              className={classes.btnAnt}
+              onClick={previousSlideHandler}
+              style={colorsBtn}
+              aria-label="item anterior"
+            >
+              <IconArrowLeft />
+            </button>
+            <button
+              className={classes.btnDep}
+              onClick={nextSlideHandler}
+              style={colorsBtn}
+              aria-label="item posterior"
+            >
+              <IconArrowRight />
+            </button>
+          </>
+        )}
+        <div className={classes.sliderContainer}>
+          <ul className={classes.slider} ref={slideRef}>
+            {items.map((item) => (
+              <ItemsSlider
+                key={item.id}
+                item={item}
+                openModal={handleOpenModal}
+                itemWidth={itemWidth}
+              />
+            ))}
+          </ul>
+        </div>
       </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
 Slider.propTypes = {
   items: PropTypes.array.isRequired,
   colorsBtn: PropTypes.exact({
     backgroundColor: PropTypes.string,
     color: PropTypes.string,
-  })
-}
+  }),
+};
 
 Slider.defaultProps = {
   colorsBtn: {
-    backgroundColor: '#ac83fa',
-    color: '#4b1688'
-  }
-}
-export default Slider
+    backgroundColor: "#ac83fa",
+    color: "#4b1688",
+  },
+};
+export default Slider;
--- src/rotas/Galeria.js
+++ src/rotas/Galeria.js
@@ -1,100 +1,109 @@
-import React, { useEffect, useState } from 'react'
-import LayoutRotas from '../components/Utility/LayoutRotas'
-import Modal from '../components/Utility/Modal';
-import classes from './Galeria.module.css';
+import React, { useEffect, useState } from "react";
+import LayoutRotas from "../components/Utility/LayoutRotas";
+import Modal from "../components/Utility/Modal";
+import classes from "./Galeria.module.css";
+import Head from "../components/Utility/Head";
 
 const galeria = [
   {
-    img: 'gal1.webp',
-    descricao: 'Donec ac malesuada tortor. Suspendisse justo enim.',
-    id: 'gal1'
+    img: "gal1.webp",
+    descricao: "Donec ac malesuada tortor. Suspendisse justo enim.",
+    id: "gal1",
   },
   {
-    img: 'gal2.webp',
-    descricao: 'Nunc a dapibus leo.',
-    id: 'gal2'
+    img: "gal2.webp",
+    descricao: "Nunc a dapibus leo.",
+    id: "gal2",
   },
   {
-    img: 'gal3.webp',
-    descricao: 'Vestibulum tristique malesuada erat ut ultricies.',
-    id: 'gal3'
+    img: "gal3.webp",
+    descricao: "Vestibulum tristique malesuada erat ut ultricies.",
+    id: "gal3",
   },
   {
-    img: 'gal4.webp',
-    descricao: 'In eu lacus lacinia nibh gravida commodo',
-    id: 'gal4'
+    img: "gal4.webp",
+    descricao: "In eu lacus lacinia nibh gravida commodo",
+    id: "gal4",
   },
   {
-    img: 'gal5.webp',
-    descricao: 'Proin ac sodales leo',
-    id: 'gal5'
+    img: "gal5.webp",
+    descricao: "Proin ac sodales leo",
+    id: "gal5",
   },
   {
-    img: 'gal6.webp',
-    descricao: 'Quisque et fermentum turpis.',
-    id: 'gal6'
+    img: "gal6.webp",
+    descricao: "Quisque et fermentum turpis.",
+    id: "gal6",
   },
   {
-    img: 'gal7.webp',
-    descricao: 'Maecenas a lobortis urna.',
-    id: 'gal7'
+    img: "gal7.webp",
+    descricao: "Maecenas a lobortis urna.",
+    id: "gal7",
   },
   {
-    img: 'gal8.webp',
-    descricao: 'Aenean consequat elit ut magna fermentum posuere.',
-    id: 'gal8'
+    img: "gal8.webp",
+    descricao: "Aenean consequat elit ut magna fermentum posuere.",
+    id: "gal8",
   },
   {
-    img: 'gal9.webp',
-    descricao: 'Praesent efficitur sit amet felis eget vehicula.',
-    id: 'gal9'
-  }
-]
+    img: "gal9.webp",
+    descricao: "Praesent efficitur sit amet felis eget vehicula.",
+    id: "gal9",
+  },
+];
 
 const Galeria = () => {
-
   const [modalShown, setModalShown] = useState(false);
-  const [currentImageShown, setCurrentImageShown] = useState()
+  const [currentImageShown, setCurrentImageShown] = useState();
 
   useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [])
-
+    window.scrollTo(0, 0);
+  }, []);
 
   const closeModal = () => {
-    setModalShown(false)
-  }
+    setModalShown(false);
+  };
 
   const openModal = (imagem) => {
     setModalShown(true);
-    setCurrentImageShown(imagem)
-  }
+    setCurrentImageShown(imagem);
+  };
 
   return (
     <>
-    {modalShown && 
-    <Modal onClose={closeModal}>
-      <div className={classes.modal}>
-        <div className={classes.img}>
-          <img src={require(`../assets/${currentImageShown.img}`)} alt={currentImageShown.descricao} />
-        </div>
-        <p>{currentImageShown.descricao}</p>
-      </div>
-    </Modal>
-    }
-    <LayoutRotas titulo='Conheça nosso ambiente!' descricao='Curabitur rutrum elit vel felis volutpat interdum'>
-      <ul className={classes.galeria}>
-        {galeria.map(imagem => 
-        <li key={imagem.id}>
-          <button onClick={() => openModal(imagem)}>
-            <img src={require(`../assets/${imagem.img}`)} alt={imagem.descricao}/>
-          </button>
-        </li> 
-          )}
-      </ul>
-    </LayoutRotas>
+      <Head title="Galeria" description="Conheça o nosso ambiente" />
+      {modalShown && (
+        <Modal onClose={closeModal}>
+          <div className={classes.modal}>
+            <div className={classes.img}>
+              <img
+                src={require(`../assets/${currentImageShown.img}`)}
+                alt={currentImageShown.descricao}
+              />
+            </div>
+            <p>{currentImageShown.descricao}</p>
+          </div>
+        </Modal>
+      )}
+      <LayoutRotas
+        titulo="Conheça nosso ambiente!"
+        descricao="Curabitur rutrum elit vel felis volutpat interdum"
+      >
+        <ul className={classes.galeria}>
+          {galeria.map((imagem) => (
+            <li key={imagem.id}>
+              <button onClick={() => openModal(imagem)}>
+                <img
+                  src={require(`../assets/${imagem.img}`)}
+                  alt={imagem.descricao}
+                />
+              </button>
+            </li>
+          ))}
+        </ul>
+      </LayoutRotas>
     </>
-  )
-}
+  );
+};
 
-export default Galeria
+export default Galeria;
